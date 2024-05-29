5b0f4734f601367b576c0c81d5c239789c41e4c2
MirulFiq <149250526+mirulfiq1@users.noreply.github.com>
2024-05-29 17:47:51 +0800

    Add files via upload

---
+++ layouts/header.php
@@ -0,0 +1,477 @@
+<?php $user = current_user(); ?>
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title><?php if (!empty($page_title))
+           echo remove_junk($page_title);
+            elseif(!empty($user))
+           echo ucfirst($user['name']);
+            else echo "Inventory Management System";?>
+    </title>
+
+    </head>
+  <body>
+  <?php  if ($session->isUserLoggedIn(true)): ?>
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Rashidah Bookstore</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    </head>
+
+<body class="hold-transition sidebar-mini layout-fixed dark-mode" data-panel-auto-height-mode="height">
+<div class="wrapper">
+<header id="yo"> 
+<nav class="main-header navbar navbar-expand navbar-dark">
+
+
+    <!-- Nav 1 -->
+    <ul class="navbar-nav">
+     <header id="header"> 
+      <div class="header-content">
+       <div class="pull-right clearfix">
+        <ul class="info-menu list-inline list-unstyled">
+         <ul class="nav homelog">
+           <li class="nav-item d-none d-sm-inline-block">
+              <a href="home.php" class="nav-link">Home</a>
+           </li>
+           <li class="info">
+              <a href="logout.php" class="nav-link">Logout</a>
+           </li>
+         </ul>
+    <li class="profile">
+      <a href="#" data-toggle="dropdown" class="toggle" aria-expanded="false">
+          <img src="uploads/users/<?php echo $user['image'];?>" alt="user-image" class="img-circle img-inline">
+          <span><?php echo remove_junk(ucfirst($user['name']));?> <i class="caret"></i></span>
+      </a>
+    <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+    <li class="nav-item">
+          <a href="profile.php?id=<?php echo (int)$user['id'];?>" class="dropdown-item">
+            <i class="glyphicon glyphicon-user"></i>
+              Profile
+          </a>
+        </li>
+        <div class="dropdown-divider"></div>
+        <li class="nav-item">
+          <a href="edit_account.php" class="dropdown-item">
+            <i class="glyphicon glyphicon-cog"></i>
+              Settings
+          </a>
+        </li>
+     </ul>
+     </li>
+        </ul>
+       </div>
+      </div>
+     </header>
+    </ul>
+
+<style>
+   .header-date {
+        padding: 10px;
+        border-radius: 5px;
+        font-size: 15px;
+        font-weight: bold;
+        display: flex;
+        align-items: center;
+    }
+ 
+    #yo {
+        position: fixed;
+        z-index: 9999;
+        top: 0;
+        left: 0;
+        width: 100%;
+    } 
+
+    #header {
+        z-index: 100;
+        top: 0;
+        left: 0;
+        width: 100%;
+    }
+
+   .profile a.toggle{
+        width: 150px; 
+        height: 35px; 
+        background-color: #f5f5f5;
+        border-radius: 50px;
+        top: 10px;
+        right: 20px;
+        padding-right: 10px;
+        padding-left: 10px;
+        float: right;
+        position: absolute;  
+    }
+
+   .profile a.toggle {
+        display: flex;
+        align-items: center;
+    }
+
+   .profile a.toggle img {
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        margin-right: 10px;
+    }
+   .profile a.toggle span {
+        font-size: 14px;
+    }
+    
+</style>
+</nav>
+
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-dark">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+    
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+    <div class="header-date pull-left">
+    <strong><?php echo date("F j, Y, g:i a");?></strong>
+    </div>
+      <!-- Messages Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-comments"></i>
+          <span class="badge badge-danger navbar-badge">3</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Brad Diesel
+                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">Call me whenever you can...</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  John Pierce
+                  <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">I got your message bro</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user2-160x160.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Ahmad Nazrin
+                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">Kalah 10 - 0 eFootball</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+        </div>
+      </li>
+  
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  
+  
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="admin.php" class="brand-link">
+      <img src="dist/img/R2.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Rashidah Bookstore</span>
+    </a>
+
+    
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-solid fa-user-tie"></i>
+              <p>
+              User Management
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="group.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Manage Groups</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="users.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Manage Users</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-solid fa-box"></i>
+              <p>
+                Products
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="product.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Manage Products</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="add_product.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Add Products</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="categorie.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Categories</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-solid fa-chart-pie"></i>
+              <p>
+                Sales
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="sales.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Manage Sales</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="add_sale.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Add Sales</p>
+                </a>
+              </li>
+
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-solid fa-store"></i>
+              <p>
+                Stocks
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="stocks.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Manage Stocks</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="add_stock.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Add Stocks</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-solid fa-users"></i>
+              <p>
+                Customers
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="customers.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Manage Customers</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="add_customer.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Add Customers</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-solid fa-file"></i>
+              <p>
+                Report
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="sales_report.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Sales By Dates</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="monthly_sales.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Monthly Sales</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="daily_sales.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Daily Sales</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-solid fa-upload"></i>
+              <p>
+                Upload
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="media.php" class="nav-link">
+                  <i class="fas fa-minus nav-icon"></i>
+                  <p>Upload Image</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper iframe-mode bg-dark" data-widget="iframe" data-auto-dark-mode="true" data-loading-screen="750">
+    <div class="nav navbar navbar-expand-lg navbar-dark border-bottom border-dark p-0">
+      <a class="nav-link bg-danger" href="#" data-widget="iframe-close">Close</a>
+      <a class="nav-link bg-dark" href="#" data-widget="iframe-scrollleft"><i class="fas fa-angle-double-left"></i></a>
+      <ul class="navbar-nav" role="tablist">
+      </ul>
+      <a class="nav-link bg-dark" href="#" data-widget="iframe-scrollright"><i class="fas fa-angle-double-right"></i></a>
+      <a class="nav-link bg-dark" href="#" data-widget="iframe-fullscreen"><i class="fas fa-expand"></i></a>
+    </div>
+    <div class="tab-content">
+      <div class="tab-empty">
+        <h2 class="display-4">No tab selected!</h2>
+        
+      </div>
+      <div class="tab-loading">
+        <div>
+          <h2 class="display-4">Tab is loading <i class="fa fa-sync fa-spin"></i></h2>
+        </div>
+      </div>
+    </div>
+  </div>
+    <!-- /.content-wrapper -->
+  <footer class="main-footer dark-mode">
+    <strong>Copyright &copy; 2024 <a href="https://businessreport.ctoscredit.com.my/oneoffreport_api/single-report/malaysia-business/SA0110453P/RASHIDAH-RASHID-ENTERPRISE">Rashidah Bookstore</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 1.0.0
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.js"></script>
+</body>
+</html>
+
+<?php endif;?>
+<div class="page">
+  <div class="container-fluid">
+  </header>
