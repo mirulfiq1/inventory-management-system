01046a4ae418026e505085b134735622f5453ba5
MirulFiq <149250526+mirulfiq1@users.noreply.github.com>
2024-05-31 22:56:03 +0800

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset = "UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name= "viewport" content=""width=device-width, initial-scale="1.0">
+    <title>Rashidah Rashid Bookstore System</title>
+    <link rel="stylesheet" href="astyle.css">
+</head>
+
+<body>
+
+    <header>
+        <a href="Homepage.html" style="width:150px;"><img class="logo" src="LOGO1.png" alt="logo"></a>
+        </a>
+        <nav class="navigation">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="#">About</a>
+                <a href="#">FAQ</a>
+                <a href="#">Contact</a>
+                <a href="#" class="active"><button>Sign up</button></a>
+                <a href="index.php" class="active"><button>Log in</button></a>
+            </nav>
+    </header>
+
+    <section class="parallax">
+        <img src="img/d1.png" id="hill1">
+        <img src="img/d2.png" id="hill4">
+        <img src="img/d3.png" id="hill5">
+        <img src="img/d4.png" id="d4">
+        <img src="img/aero.png" id="leaf">
+        <img src="img/d5.png" id="plant">
+        <h2 id="text">Rashidah Rashid</h2>
+        <h2 id="text2"><br><br> Bookstore System</h2>
+    </section>
+
+    <section class="sec">
+        <h2>Parallax Scrolling Website</h2>
+        <p>Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It's not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.”
+
+            As Cicero would put it, “Um, not so fast.”
+            
+            The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin.
+            
+            Richard McClintock, a Latin scholar from Hampden-Sydney College, is credited with discovering the source behind the ubiquitous filler text. In seeing a sample of lorem ipsum, his interest was piqued by consectetur—a genuine, albeit rare, Latin word. Consulting a Latin dictionary led McClintock to a passage from De Finibus Bonorum et Malorum (“On the Extremes of Good and Evil”), a first-century B.C. text from the Roman philosopher Cicero.
+            
+            In particular, the garbled words of lorem ipsum bear an unmistakable resemblance to sections 1.10.3233 of Cicero's work, with the most notable passage excerpted below:
+            
+            “Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.”
+            A 1914 English translation by Harris Rackham reads:
+            
+            “Nor is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure.”
+            McClintock's eye for detail certainly helped narrow the whereabouts of lorem ipsum's origin, however, the “how and when” still remain something of a mystery, with competing theories and timelines.</p>
+        <br>
+        <p>Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It's not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.”
+
+        As Cicero would put it, “Um, not so fast.”
+        
+        The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin.
+        
+        Richard McClintock, a Latin scholar from Hampden-Sydney College, is credited with discovering the source behind the ubiquitous filler text. In seeing a sample of lorem ipsum, his interest was piqued by consectetur—a genuine, albeit rare, Latin word. Consulting a Latin dictionary led McClintock to a passage from De Finibus Bonorum et Malorum (“On the Extremes of Good and Evil”), a first-century B.C. text from the Roman philosopher Cicero.
+        
+        In particular, the garbled words of lorem ipsum bear an unmistakable resemblance to sections 1.10.3233 of Cicero's work, with the most notable passage excerpted below:
+        
+        “Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.”
+        A 1914 English translation by Harris Rackham reads:
+        
+        “Nor is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure.”
+        McClintock's eye for detail certainly helped narrow the whereabouts of lorem ipsum's origin, however, the “how and when” still remain something of a mystery, with competing theories and timelines.
+
+        
+        </p>
+    </section>
+
+
+        <table class= "tab"  width="1410" height="10">
+        <tr>
+        <td>
+        <p style="text-align: center"><b>Rashidah Rashid Bookstore</b></p>
+        <p style="text-align: center">Rashidahbookstore@gmail.com</p>
+        <p style="text-align: center">019-7357903</p>
+        <p style="text-align: center">No.4 Jalan raya 1, Alam Budiman, Shah Alam, Selangor</p>
+        <br>
+        <pre style="text-align: center" width="40"> <a href="https://twitter.com/Amrulx1?t=zEbRFQ3VCx-pVkLQB_2WlQ&s=09"title="Twitter"><img src="t.png"
+        width="30" height="30"></a>  <a href="https://www.instagram.com/invites/contact/?i=pd52x1fc5379&utm_content=5gunlaj"title="Facebook"><img src="f.png"
+        width="30" height="30"></a>  <a href="https://www.facebook.com/amirul.afiq.7798"title="Instagram"><img src="i.png"
+        width="30" height="30"></a>
+        </pre>
+        <br>
+        <p style="text-align: center" style="color:grey">2024 Rashidah Rashid Enterprise</p>
+        <br>
+        </td>
+        </tr>
+        </table>
+
+
+
+
+<script src="ascript.js"></script>        
+        
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,103 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+  ob_start();
+  require_once('includes/load.php');
+  if($session->isUserLoggedIn(true)) { redirect('admin.php', false);}
+?>
 
-<head>
-    <meta charset = "UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name= "viewport" content=""width=device-width, initial-scale="1.0">
-    <title>Rashidah Rashid Bookstore System</title>
-    <link rel="stylesheet" href="astyle.css">
-</head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
+    <link rel="stylesheet" href="libs/css/main.css" />
 
-<body>
-
-    <header>
-        <a href="Homepage.html" style="width:150px;"><img class="logo" src="LOGO1.png" alt="logo"></a>
-        </a>
-        <nav class="navigation">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <a href="#">About</a>
-                <a href="#">FAQ</a>
-                <a href="#">Contact</a>
-                <a href="#" class="active"><button>Sign up</button></a>
-                <a href="login.php" class="active"><button>Log in</button></a>
-            </nav>
-    </header>
-
-    <section class="parallax">
-        <img src="img/d1.png" id="hill1">
-        <img src="img/d2.png" id="hill4">
-        <img src="img/d3.png" id="hill5">
-        <img src="img/d4.png" id="d4">
-        <img src="img/aero.png" id="leaf">
-        <img src="img/d5.png" id="plant">
-        <h2 id="text">Rashidah Rashid</h2>
-        <h2 id="text2"><br><br> Bookstore System</h2>
-    </section>
-
-    <section class="sec">
-        <h2>Parallax Scrolling Website</h2>
-        <p>Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It's not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.”
-
-            As Cicero would put it, “Um, not so fast.”
-            
-            The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin.
-            
-            Richard McClintock, a Latin scholar from Hampden-Sydney College, is credited with discovering the source behind the ubiquitous filler text. In seeing a sample of lorem ipsum, his interest was piqued by consectetur—a genuine, albeit rare, Latin word. Consulting a Latin dictionary led McClintock to a passage from De Finibus Bonorum et Malorum (“On the Extremes of Good and Evil”), a first-century B.C. text from the Roman philosopher Cicero.
-            
-            In particular, the garbled words of lorem ipsum bear an unmistakable resemblance to sections 1.10.3233 of Cicero's work, with the most notable passage excerpted below:
-            
-            “Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.”
-            A 1914 English translation by Harris Rackham reads:
-            
-            “Nor is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure.”
-            McClintock's eye for detail certainly helped narrow the whereabouts of lorem ipsum's origin, however, the “how and when” still remain something of a mystery, with competing theories and timelines.</p>
-        <br>
-        <p>Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It's not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.”
-
-        As Cicero would put it, “Um, not so fast.”
-        
-        The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin.
-        
-        Richard McClintock, a Latin scholar from Hampden-Sydney College, is credited with discovering the source behind the ubiquitous filler text. In seeing a sample of lorem ipsum, his interest was piqued by consectetur—a genuine, albeit rare, Latin word. Consulting a Latin dictionary led McClintock to a passage from De Finibus Bonorum et Malorum (“On the Extremes of Good and Evil”), a first-century B.C. text from the Roman philosopher Cicero.
-        
-        In particular, the garbled words of lorem ipsum bear an unmistakable resemblance to sections 1.10.3233 of Cicero's work, with the most notable passage excerpted below:
-        
-        “Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.”
-        A 1914 English translation by Harris Rackham reads:
-        
-        “Nor is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure.”
-        McClintock's eye for detail certainly helped narrow the whereabouts of lorem ipsum's origin, however, the “how and when” still remain something of a mystery, with competing theories and timelines.
-
-        
-        </p>
-    </section>
-
-
-        <table class= "tab"  width="1410" height="10">
-        <tr>
-        <td>
-        <p style="text-align: center"><b>Rashidah Rashid Bookstore</b></p>
-        <p style="text-align: center">Rashidahbookstore@gmail.com</p>
-        <p style="text-align: center">019-7357903</p>
-        <p style="text-align: center">No.4 Jalan raya 1, Alam Budiman, Shah Alam, Selangor</p>
-        <br>
-        <pre style="text-align: center" width="40"> <a href="https://twitter.com/Amrulx1?t=zEbRFQ3VCx-pVkLQB_2WlQ&s=09"title="Twitter"><img src="t.png"
-        width="30" height="30"></a>  <a href="https://www.instagram.com/invites/contact/?i=pd52x1fc5379&utm_content=5gunlaj"title="Facebook"><img src="f.png"
-        width="30" height="30"></a>  <a href="https://www.facebook.com/amirul.afiq.7798"title="Instagram"><img src="i.png"
-        width="30" height="30"></a>
-        </pre>
-        <br>
-        <p style="text-align: center" style="color:grey">2024 Rashidah Rashid Enterprise</p>
-        <br>
-        </td>
-        </tr>
-        </table>
-
-
-
-
-<script src="ascript.js"></script>        
-        
-</body>
-</html>
+<div class="login-page">
+    <div class="text-center">
+       <h1>Login Panel</h1>
+       <h4>Inventory Management System</h4>
+     </div>
+     <?php echo display_msg($msg); ?>
+      <form method="post" action="auth.php" class="clearfix">
+        <div class="form-group">
+              <label for="username" class="control-label">Username</label>
+              <input type="name" class="form-control" name="username" placeholder="Username">
+        </div>
+        <div class="form-group">
+            <label for="Password" class="control-label">Password</label>
+            <input type="password" name= "password" class="form-control" placeholder="Password">
+        </div>
+        <div class="form-group">
+                <button type="submit" class="btn btn-danger" style="border-radius:0%">Login</button>
+        </div>
+    </form>
+</div>
+<?php include_once('layouts/footer.php'); ?>
